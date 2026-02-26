08082bc712e0bb9782f27ee38c6c22698c78c03e
Neel Kolhe <89089255+Neel-Kolhe@users.noreply.github.com>
2026-02-26 17:34:22 +0100

    Update data.js

--- data.js
+++ data.js
@@ -136,7 +136,7 @@ const SITE = {
       label: "Other Writings",
       sidebar: {
         
-        left:  "assets/bruckner-4.jpg",//null, // e.g. "assets/milkyway.jpg"
+        left:  "assets/voyager1.jpg",//null, // e.g. "assets/milkyway.jpg"
         right: "assets/sibe_bar_flower2.jpg",//null,
       },
       content: {
