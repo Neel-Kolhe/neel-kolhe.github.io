fe5df9904e1b38e42054cbb5edd6ceb53d5e1ea5
Neel Kolhe <89089255+Neel-Kolhe@users.noreply.github.com>
2026-02-26 19:54:53 +0100

    Update data.js

--- data.js
+++ data.js
@@ -46,7 +46,7 @@ const SITE = {
       label: "Home",
       sidebar: {
         left:  "assets/bruckner-4.jpg",               // e.g. "assets/milkyway.jpg"
-        right: "assets/collage1.png",//null,
+        right: "assets/collage1.jpg",//null,
       },
       content: {
         type: "home",
